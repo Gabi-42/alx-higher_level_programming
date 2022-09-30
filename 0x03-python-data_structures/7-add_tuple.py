58f1efe05e182fa712c269c85388d236432ec6ac
Gabriella Peji <omotpeji@gmail.com>
2022-09-30 09:26:53 -0700

    alx task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-    if len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-    if len(tuple_a) = 0:
-        tuple_a = (0, 0)
-    if len(tuple_b) > 2:
-        tuple_b = (tuple_b[0], tuple_b[1])
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    if len(tuple_b) = 0:
-        tuple_b = (0, 0)
-    sum_tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+def add_tuple(tuplea=(), tupleb=()):
+    if len(tuplea) > 2:
+        tuplea = (tuplea[0], tuplea[1])
+    if len(tuplea) == 1:
+        tuplea = (tuplea[0], 0)
+    if len(tuplea) = 0:
+        tuplea = (0, 0)
+    if len(tupleb) > 2:
+        tupleb = (tupleb[0], tupleb[1])
+    if len(tupleb) == 1:
+        tupleb = (tupleb[0], 0)
+    if len(tupleb) = 0:
+        tupleb = (0, 0)
+    sum_tup = (tuplea[0] + tupleb[0], tuplea[1] + tupleb[1])
     return sum_tup
